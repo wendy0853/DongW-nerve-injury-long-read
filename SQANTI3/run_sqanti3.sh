5c7da63688bee7d4acce82d265eb3fb788d906ee
Wendy Dong <103447014+wendy0853@users.noreply.github.com>
2025-09-18 12:12:17 -0500

    Update run_sqanti3.sh

--- SQANTI3/run_sqanti3.sh
+++ SQANTI3/run_sqanti3.sh
@@ -12,8 +12,8 @@ SHORT_READ_BAM="$(echo /storage1/fs1/jin810/Active/Projects/RNAseq/Long_read_RNA
 OUTPUT_DIR="$(echo /storage1/fs1/jin810/Active/Projects/RNAseq/Long_read_RNAseq_Crush_Injury/Long_read/${DIR}/SQANTI3_Output_updated/${SAMPLE_NAME})"
 
 
-REFERENCE_GTF="/storage1/fs1/jin810/Active/References/GRCm39/gencode.vM38.chr_patch_hapl_scaff.annotation.gtf"
-REFERENCE_FASTA="/storage1/fs1/jin810/Active/References/GRCm39/mm38.fa"
+REFERENCE_GTF="/storage1/fs1/jin810/Active/References/GRCm39/gencode.vM38.chr_patch_hapl_scaff.annotation.gtf.gz"
+REFERENCE_FASTA="/storage1/fs1/jin810/Active/References/GRCm39/GRCm39.genome.fa"
 POLYA_MOTIF_LIST="/opt2/sqanti3/5.3.6/SQANTI3-5.3.6/data/polyA_motifs/mouse_and_human.polyA_motif.txt"
 CAGE_PEAK_FILE="/opt2/sqanti3/5.3.6/SQANTI3-5.3.6/data/ref_TSS_annotation/mouse.refTSS_v3.1.GRCm39.bed"
 
