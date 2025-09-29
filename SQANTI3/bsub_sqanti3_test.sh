ec0e9986b681fd0c67e191f197644fb448df789d
Wendy Dong <103447014+wendy0853@users.noreply.github.com>
2025-09-29 14:18:12 -0500

    Update bsub_sqanti3_test.sh

--- SQANTI3/bsub_sqanti3_test.sh
+++ SQANTI3/bsub_sqanti3_test.sh
@@ -1,3 +1,11 @@
-export LSF_DOCKER_VOLUMES='/storage1/fs1/jin810/Active/References:/storage1/fs1/jin810/Active/References /storage1/fs1/jin810/Active/Projects:/storage1/fs1/jin810/Active/Projects /home/d.wendy:/home/d.wendy'
+##RUN CODE: bash bsub_sqanti3_test.sh {SAMPLE_NAME} {DIR} {SRB}
 
-LSF_DOCKER_PRESERVE_ENVIRONMENT=false bsub -q general -n 16 -M 64GB -R "rusage[mem=64GB] span[hosts=1]" -G compute-jin810 -a 'docker(etycksen/isoquant:latest)' /bin/bash run_isoquant.sh
+export LSF_DOCKER_VOLUMES='/storage1/fs1/jin810/Active/References:/storage1/fs1/jin810/Active/References /storage1/fs1/jin810/Active/Projects:/storage1/fs1/jin810/Active/Projects /home/d.wendy:/home/d.wendy'
+#-o %J.SQANTI3_C7_Injured_Sciatic_1.out -e %J.SQANTI_C7_Injured_Sciatic_1.err 
+#C7_Injured_Sciatic_1
+SAMPLE_NAME=$1
+#C7_Injury
+DIR=$2
+#fastq_C7_Injured_1.fofn
+SRB=$3
+LSF_DOCKER_PRESERVE_ENVIRONMENT=false bsub  -G compute-jin810 -q general -n 16  -R 'rusage[mem=100GB]' -a 'docker(jinlab/sqanti3:vs1)' /bin/bash run_sqanti3.sh ${SAMPLE_NAME} ${DIR} ${SRB}
