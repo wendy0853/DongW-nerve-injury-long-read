b3038fa34b41bc46e2fc8bf697264b6fa1363e54
Wendy Dong <103447014+wendy0853@users.noreply.github.com>
2025-09-29 15:36:10 -0500

    Update and rename bsub_sqanti3_qc_test.sh to run_sqanti3_qc_test.sh

--- SQANTI3/run_sqanti3_qc_test.sh
+++ SQANTI3/run_sqanti3_qc_test.sh
@@ -19,6 +19,7 @@ REFERENCE_FASTA="/storage1/fs1/jin810/Active/References/GRCm39/GRCm39.genome.fa"
 POLYA_MOTIF_LIST="/opt2/sqanti3/5.3.6/SQANTI3-5.3.6/data/polyA_motifs/mouse_and_human.polyA_motif.txt"
 CAGE_PEAK_FILE="/opt2/sqanti3/5.3.6/SQANTI3-5.3.6/data/ref_TSS_annotation/mouse.refTSS_v3.1.GRCm39.bed"
 POLYA_PEAK_FILE="/storage1/fs1/jin810/Active/References/GRCm39/polyA_mm39_lifted.bed"
+FL_COUNT="/storage1/fs1/jin810/Active/Projects/RNAseq/Long_read_RNAseq_Crush_Injury/Long_read/${DIR}/IsoQuant_Results_updated/${SAMPLE_NAME}/${SAMPLE_NAME}.transcript_model_counts.tsv"
 
 # Create output directory
 mkdir -p ${OUTPUT_DIR}
@@ -27,4 +28,4 @@ mkdir -p ${OUTPUT_DIR}
 
 # Run SQANTI3 QC with CAGE peaks and polyA peaks
 echo ${SAMPLE_NAME} ${ISOQUANT_GTF} ${OUTPUT_DIR}
-sqanti3_qc.py ${ISOQUANT_GTF} ${REFERENCE_GTF} ${REFERENCE_FASTA} --fastq ${RAW_FASTQ} --short_reads ${SHORT_READ_BAM}   --polyA_motif_list ${POLYA_MOTIF_LIST}   --polyA_peak ${POLYA_PEAK_FILE}   --CAGE_peak ${CAGE_PEAK_FILE}   -d ${OUTPUT_DIR}   --cpus 8 -o ${SAMPLE_NAME} 
+sqanti3_qc.py ${ISOQUANT_GTF} ${REFERENCE_GTF} ${REFERENCE_FASTA} --fasta ${RAW_FASTQ} --short_reads ${SHORT_READ_BAM}   --polyA_motif_list ${POLYA_MOTIF_LIST}   --polyA_peak ${POLYA_PEAK_FILE}   --CAGE_peak ${CAGE_PEAK_FILE}   -fl ${FL_COUNT}   -d ${OUTPUT_DIR}   --cpus 8 -o ${SAMPLE_NAME} 
