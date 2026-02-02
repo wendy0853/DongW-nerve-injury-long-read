8b9a73647e61c1114ba5a85fc9c4265da5f241ac
Wendy Dong <103447014+wendy0853@users.noreply.github.com>
2026-02-02 11:10:51 -0600

    Add bsub script for SQANTI3 rescue rules

---
+++ SQANTI3/bsub_sqanti3_rescue_rules_automatic.sh
@@ -0,0 +1,8 @@
+##RUN CODE: bash bsub_sqanti3_rescue_rules_automatic.sh {SAMPLE_NAME} {DIR}
+export LSF_DOCKER_VOLUMES='/storage1/fs1/jin810/Active/References:/storage1/fs1/jin810/Active/References /storage1/fs1/jin810/Active/Projects:/storage1/fs1/jin810/Active/Projects /home/d.wendy:/home/d.wendy'
+#-o %J.SQANTI3_C7_Injured_Sciatic_1.out -e %J.SQANTI_C7_Injured_Sciatic_1.err 
+#C7_Injured_Sciatic_1
+SAMPLE_NAME=$1
+#C7_Injury
+DIR=$2
+LSF_DOCKER_PRESERVE_ENVIRONMENT=false bsub  -G compute-jin810-t3 -q subscription -sla jin810_t3 -n 16  -R 'rusage[mem=100GB]' -a 'docker(jinlab/sqanti3:vs1)' /bin/bash run_sqanti3_rescue_rules_automatic.sh ${SAMPLE_NAME} ${DIR}
