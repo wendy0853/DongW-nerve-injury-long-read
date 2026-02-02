9f7de9ee38635dfbd1f1a818d010a06965dc9eed
Wendy Dong <103447014+wendy0853@users.noreply.github.com>
2026-02-02 11:07:37 -0600

    Change output directory and filter method in script

--- SQANTI3/run_sqanti3_rescue_rules_automatic.sh
+++ SQANTI3/run_sqanti3_rescue_rules_automatic.sh
@@ -7,7 +7,7 @@ DIR=$2
 CLASS_TXT="$(echo /storage1/fs1/jin810/Active/Projects/RNAseq/Long_read_RNAseq_Crush_Injury/Long_read/${DIR}/SQANTI3_Output_updated/${SAMPLE_NAME}/${SAMPLE_NAME}_classification.txt)"
 CORRECTED_FASTA="$(echo /storage1/fs1/jin810/Active/Projects/RNAseq/Long_read_RNAseq_Crush_Injury/Long_read/${DIR}/SQANTI3_Output_updated/${SAMPLE_NAME}/${SAMPLE_NAME}_corrected.fasta)"
 CORRECTED_GTF="$(echo /storage1/fs1/jin810/Active/Projects/RNAseq/Long_read_RNAseq_Crush_Injury/Long_read/${DIR}/SQANTI3_Output_updated/${SAMPLE_NAME}/${SAMPLE_NAME}_corrected.gtf)"
-OUTPUT_DIR="$(echo /storage1/fs1/jin810/Active/Projects/RNAseq/Long_read_RNAseq_Crush_Injury/Long_read/${DIR}/SQANTI3_Output_updated/${SAMPLE_NAME}/filtered_ml)"
+OUTPUT_DIR="$(echo /storage1/fs1/jin810/Active/Projects/RNAseq/Long_read_RNAseq_Crush_Injury/Long_read/${DIR}/SQANTI3_Output_updated/${SAMPLE_NAME}/filtered_rules)"
 REFERENCE_GTF="/storage1/fs1/jin810/Active/References/GRCm39/gencode.vM38.chr_patch_hapl_scaff.annotation.gtf.gz"
 REFERENCE_FASTA="/storage1/fs1/jin810/Active/References/GRCm39/GRCm39.genome.fa"
 POLYA_MOTIF_LIST="/opt2/sqanti3/5.3.6/SQANTI3-5.3.6/data/polyA_motifs/mouse_and_human.polyA_motif.txt"
@@ -18,7 +18,7 @@ mkdir -p ${OUTPUT_DIR}
 # Run SQANTI3 Filter
 echo ${SAMPLE_NAME} ${CLASS_TXT} ${OUTPUT_DIR}
 
-sqanti3_rescue.py ml --filter_class  ${OUTPUT_DIR} \
+sqanti3_rescue.py rules --filter_class  ${OUTPUT_DIR} \
                         --refGTF ${REFERENCE_GTF} \
                         --refFasta ${REFERENCE_FASTA} \
                         --mode automatic \
