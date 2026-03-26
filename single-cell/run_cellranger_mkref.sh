d6d80b6855c707e4ddd602c0ac4c4ed12722dce1
Wendy Dong <103447014+wendy0853@users.noreply.github.com>
2026-03-25 19:41:38 -0500

    Increase memory allocation from 200GB to 300GB

--- single-cell/run_cellranger_mkref.sh
+++ single-cell/run_cellranger_mkref.sh
@@ -5,10 +5,11 @@ LSF_DOCKER_PRESERVE_ENVIRONMENT=false bsub \
   -q subscription \
   -sla jin810_t3 \
   -n 16 \
-  -M 200GB \
+  -M 300GB \
   -R 'span[hosts=1] rusage[mem=150GB]' \
   -a 'docker(jinlab/velocytoxcellranger:vs0.17.17x9.0.1)' \
   cellranger mkref \
     --genome=GRCm39_MbpSplit \
     --fasta=/storage1/fs1/jin810/Active/References/GRCm39_10X/refdata-gex-GRCm39-2024-A/fasta/genome.fa \
     --genes=/storage1/fs1/jin810/Active/References/GRCm39_10X/genes.MbpSplit.gtf
+     --memgb=64
