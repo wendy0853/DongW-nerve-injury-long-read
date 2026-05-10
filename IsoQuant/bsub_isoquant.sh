6615d8bd5a7f8872bc3444dd2f55d4f62f93cd4b
Wendy Dong <103447014+wendy0853@users.noreply.github.com>
2026-05-10 11:26:43 -0500

    Delete IsoQuant/bsub_isoquant.sh

--- IsoQuant/bsub_isoquant.sh
+++
@@ -1,3 +0,0 @@
-export LSF_DOCKER_VOLUMES='/storage1/fs1/jin810/Active/References:/storage1/fs1/jin810/Active/References /storage1/fs1/jin810/Active/Projects:/storage1/fs1/jin810/Active/Projects /home/d.wendy:/home/d.wendy'
-
-LSF_DOCKER_PRESERVE_ENVIRONMENT=false bsub -q general -n 16 -M 64GB -R "rusage[mem=64GB] span[hosts=1]" -G compute-jin810 -a 'docker(etycksen/isoquant:latest)' /bin/bash run_isoquant.sh
