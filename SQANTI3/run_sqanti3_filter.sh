9df617aa705e25b40dd4d3cf81b4ca56ecd98eca
Wendy Dong <103447014+wendy0853@users.noreply.github.com>
2026-05-10 11:49:09 -0500

    Refactor run_sqanti3_filter.sh with user-defined paths
    Updated the script to include user-defined paths and improved comments for clarity.

--- SQANTI3/run_sqanti3_filter.sh
+++ SQANTI3/run_sqanti3_filter.sh
@@ -1,17 +1,45 @@
-##!/bin/bash
-#C7_Injured_Sciatic_1
+#!/bin/bash
+
+# =========================
+# SQANTI3 QC
+# Executed inside Docker container:
+# jinlab/sqanti3:vs1
+# =========================
+
+set -euo pipefail
+
+# Usage:
+# bash run_sqanti3_qc.sh SAMPLE_NAME
+
 SAMPLE_NAME=$1
-#C7_Injury
-DIR=$2
 
-CLASS_TXT="$(echo /storage1/fs1/jin810/Active/Projects/RNAseq/Long_read_RNAseq_Crush_Injury/Long_read/${DIR}/SQANTI3_Output_updated/${SAMPLE_NAME}/${SAMPLE_NAME}_classification.txt)"
-CORRECTED_FASTA="$(echo /storage1/fs1/jin810/Active/Projects/RNAseq/Long_read_RNAseq_Crush_Injury/Long_read/${DIR}/SQANTI3_Output_updated/${SAMPLE_NAME}/${SAMPLE_NAME}_corrected.fasta)"
-CORRECTED_GTF="$(echo /storage1/fs1/jin810/Active/Projects/RNAseq/Long_read_RNAseq_Crush_Injury/Long_read/${DIR}/SQANTI3_Output_updated/${SAMPLE_NAME}/${SAMPLE_NAME}_corrected.gtf)"
-OUTPUT_DIR="$(echo /storage1/fs1/jin810/Active/Projects/RNAseq/Long_read_RNAseq_Crush_Injury/Long_read/${DIR}/SQANTI3_Output_updated/${SAMPLE_NAME}/filtered)"
+# =========================
+# User-defined paths
+# =========================
 
-# Create output directory
-mkdir -p ${OUTPUT_DIR}
-# Run SQANTI3 Filter
-echo ${SAMPLE_NAME} ${CLASS_TXT} ${OUTPUT_DIR}
+ISOQUANT_GTF="/path/to/IsoQuant/output/${SAMPLE_NAME}.transcript_models.gtf"
+SHORT_READS="/path/to/short_read_alignments_or_fofn/${SAMPLE_NAME}.fofn"
+OUTPUT_DIR="/path/to/SQANTI3/output/${SAMPLE_NAME}"
 
-sqanti3_filter.py --sqanti_class ${CLASS_TXT} --filter_gtf ${CORRECTED_GTF} --dir ${OUTPUT_DIR} -o ${SAMPLE_NAME}_filtered
+REFERENCE_GTF="/path/to/reference/gencode.vM38.annotation.gtf"
+REFERENCE_FASTA="/path/to/reference/GRCm39.genome.fa"
+
+POLYA_MOTIF_LIST="/path/to/SQANTI3/data/polyA_motifs/mouse_and_human.polyA_motif.txt"
+CAGE_PEAK_FILE="/path/to/SQANTI3/data/ref_TSS_annotation/mouse.refTSS_v3.1.GRCm39.bed"
+
+# =========================
+# Run SQANTI3 QC
+# =========================
+
+mkdir -p "${OUTPUT_DIR}"
+
+sqanti3_qc.py \
+"${ISOQUANT_GTF}" \
+"${REFERENCE_GTF}" \
+"${REFERENCE_FASTA}" \
+--short_reads "${SHORT_READS}" \
+--polyA_motif_list "${POLYA_MOTIF_LIST}" \
+--CAGE_peak "${CAGE_PEAK_FILE}" \
+-d "${OUTPUT_DIR}" \
+--cpus 8 \
+-o "${SAMPLE_NAME}"
